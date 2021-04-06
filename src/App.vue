3cfd6afb7d434e197db58f758ded244faa1bca55
Patrick-web <pntxall100@gmail.com>
2021-04-06 12:51:18 +0300

    The most minimal form of the website

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="app">
-    <div v-if="!showPitchDeck" class="landing">
-      <div class="logoWrapper animated zoomIn">
-        <img id="logo" src="@/assets/images/FLB logo.svg" alt="" />
-      </div>
+    <div class="landing">
       <h1 class="hidden">FLB Music</h1>
       <div class="animated fadeInUp" id="headingTitle">
-        <img src="@/assets/images/FLB Double Logo.svg" alt="" />
-        <h1>LB Music</h1>
-      </div>
-      <div class="subHeading animated fadeInUp delay-1s">
+        <div class="heading">
+          <img src="@/assets/images/FLB Double Logo.svg" alt="" />
+          <h1>LB Music</h1>
+        </div>
         <h3>
           The
           <span id="nextText"
@@ -19,59 +16,22 @@
         </h3>
         <h4>In Music 🎶 though</h4>
       </div>
-      <div class="firstPreviewWrapper">
-        <img
-          id="firstPreview"
-          class="animated fadeInUp delay-1s"
-          src="@/assets/images/flb promo.png"
-          alt="FLB Preview"
-        />
-      </div>
-
-      <div class="bottomContent">
-        <a
-          class="downloadButton animated fadeInUp delay-1s"
-          title="Download FLB Music"
-          href="https://github.com/Patrick-web/FLB-Music-Player-Official/releases/download/v1.0.0/FLB-Music-Setup-1.0.0.exe"
-        >
-          <img src="@/assets/images/windows.svg" alt="" />
-          <p>Download</p>
-        </a>
-      </div>
     </div>
-    <button
-      @click="showPitchDeck = true"
-      id="doubt"
-      class="animated fadeInUp delay-2s"
-      v-if="!showPitchDeck"
-    >
-      <p>Naah, I don't believe you</p>
+    <div class="previews">
+      <Previews />
+    </div>
+    <button class="downloadButton">
+      <img src="@/assets/images/windows.svg" alt="" />
+      <p>Download</p>
     </button>
-    <button
-      id="believe"
-      class="animated tada delay-10s"
-      v-if="showPitchDeck"
-      @click="showPitchDeck = false"
-    >
-      Close
-    </button>
-
-    <transition enter-active-class="animated fadeInUp">
-      <pitch-deck v-if="showPitchDeck" />
-    </transition>
   </div>
 </template>
 
 <script>
-import PitchDeck from "./components/PitchDeck.vue";
+import Previews from "./components/Previews.vue";
 export default {
   name: "App",
-  data() {
-    return {
-      showPitchDeck: false,
-    };
-  },
-  components: { PitchDeck },
+  components: { Previews },
 };
 </script>
 
@@ -82,71 +42,53 @@ export default {
 #app {
   height: 100vh;
   width: 100vw;
-  overflow: hidden;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: white;
-  background: linear-gradient(45deg, #503d26, #472c1f, #403131);
+  background: linear-gradient(306deg, #284854, #492e35) !important;
   transition: 0.2s ease;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
   @media (max-width: 700px) {
-    overflow-y: scroll;
-  }
-}
-.logoWrapper {
-  margin: auto;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  background: white;
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  place-items: center;
-  display: none;
-  @media (min-width: 700px) {
-    display: grid;
-    position: fixed;
-    top: 20px;
-    left: 20px;
-    margin: 0px;
-  }
-  #logo {
-    width: 30px;
-    height: 30px;
+    display: flex;
+    flex-direction: column;
   }
 }
 .hidden {
   display: none;
 }
 #headingTitle {
-  padding: 5px;
   display: flex;
-  align-items: baseline;
-  justify-content: center;
-  margin-top: 10px;
-  img {
-    width: 2.5rem;
-    margin-left: 5px;
-  }
-  h1 {
-    font-size: 2.5rem;
-    font-family: roboto-thick;
-    transform: translateY(-10px) translateX(-10px);
-    text-shadow: 2.5px 2.5px rgba(255, 255, 255, 0.219);
-  }
-  @media (min-width: 700px) {
-    position: absolute;
-    top: 30%;
-    left: 40px;
-    z-index: 2;
-    margin-top: 0px;
-    h1 {
-      font-size: 6rem;
-    }
+  flex-direction: column;
+  align-items: flex-start;
+  .heading {
+    padding: 5px;
+    display: flex;
+    align-items: baseline;
+    justify-content: center;
+    margin-top: 10px;
     img {
-      width: 70px;
+      width: 5rem;
     }
+    h1 {
+      font-size: 5rem;
+      font-family: roboto-thick;
+      transform: translateY(-10px) translateX(-10px);
+      text-shadow: 2.5px 2.5px rgba(255, 255, 255, 0.219);
+    }
+    @media (max-width: 700px) {
+      img {
+        width: 3rem;
+      }
+      h1 {
+        font-size: 3rem;
+      }
+    }
+  }
+  @media (max-width: 700px) {
+    align-items: center;
   }
 }
 
@@ -161,67 +103,32 @@ export default {
   transform: translateY(-80%) translateX(80%);
 }
 
-.subHeading {
-  margin-left: 40px;
-  margin-top: 10px;
-  h3 {
-    font-size: 1.1rem;
-  }
+.landing {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   h3,
   h4 {
     text-align: left;
-    font-weight: 400;
+    font-weight: 300;
+  }
+  h3 {
+    font-size: 2rem;
   }
   h4 {
-    font-family: "Handlee", cursive;
+    font-size: 1.3rem;
+    font-family: roboto-thin;
   }
-  @media (min-width: 700px) {
-    position: absolute;
-    top: 58%;
-    left: 40px;
-    transform: translateY(-50%);
-    z-index: 2;
-    margin: 0px;
-    h3 {
-      font-size: 1.8rem;
-    }
+  @media (max-width: 700px) {
+    margin-bottom: 20px;
     h4 {
-      font-size: 1.4rem;
+      font-size: 1.1rem;
     }
-  }
-}
-#firstPreview {
-  margin-top: 20px;
-  width: 100vw;
-  @media (min-width: 700px) {
-    position: absolute;
-    top: 10%;
-    height: 65%;
-    width: auto;
-    margin: 0px;
-    right: 20px;
-  }
-}
-
-.bottomContent {
-  position: absolute;
-  bottom: 20px;
-  @media (min-width: 700px) {
-    left: 50%;
-    transform: translateX(-50%);
-  }
-}
-.mobileUsersMsg {
-  margin-top: 10px;
-  p {
-    text-align: left;
-    margin-left: 40px;
-    font-family: roboto-light;
-    font-size: 1.2rem;
-    margin-top: 10px;
-  }
-  @media (min-width: 700px) {
-    display: none;
+    h3 {
+      font-size: 1.2rem;
+    }
   }
 }
 .downloadButton {
@@ -232,86 +139,41 @@ export default {
   justify-content: space-evenly;
   background: white;
   padding: 10px;
-  border-radius: 15px;
+  padding-right: 20px;
+  padding-left: 20px;
+  border-radius: 2rem;
   position: fixed;
-  bottom: 10px;
-  left: 10px;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
   transition: 0.2s ease-in-out;
   p {
     color: var(--accentColor);
     font-family: roboto-thick;
-    font-size: 1rem;
+    font-size: 2rem;
   }
   img {
-    width: 15px;
+    width: 1.5rem;
     margin-right: 10px;
   }
   &:hover {
-    border-radius: 30px;
+    border-radius: 1rem;
   }
-  @media (min-width: 700px) {
-    width: 200px;
-    p {
-      font-size: 1.6rem;
-    }
-    img {
-      width: 25px;
-    }
-  }
-}
-#doubt {
-  background: rgba(255, 255, 255, 0.26);
-  padding: 8px;
-  border-radius: 10px;
-  position: fixed;
-  bottom: 15px;
-  right: 10px;
-  p {
-    font-family: roboto-light;
-  }
-  img {
-    width: 100px;
-    opacity: 0;
-    transform: translateY(20px);
-    transition: 0.2s ease;
-  }
-  &:hover {
-    img {
-      opacity: 1;
-      transform: translateY(0px);
-    }
-  }
-  @media (min-width: 700px) {
-    right: 40px;
-    bottom: 30px;
-    font-size: 1.4rem;
-    width: 300px;
-  }
-}
-#believe {
-  position: fixed;
-  top: 10px;
-  right: 40px;
-  z-index: 3;
-  background: rgba(255, 255, 255, 0.363);
-  padding: 8px;
-  padding-right: 10px;
-  padding-left: 10px;
-  color: white;
-  backdrop-filter: blur(10px);
-  border-radius: 20px;
-  transition: 0.2s ease;
   @media (max-width: 700px) {
-    top: initial;
-    bottom: 20px;
-    right: initial;
-    left: 40%;
-    transform: translateX(-50%);
+    p {
+      font-size: 1.2rem;
+      img {
+        width: 1rem;
+      }
+    }
   }
-  &:hover {
-    background: var(--accentColor);
-    color: white;
-    border-radius: 10px;
+}
+.previews {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 700px) {
+    height: 60%;
   }
 }
 </style>
---
+++ src/components/Previews.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="preview">
+    <img id="laptop" src="@/assets/images/laptop.png" alt="" />
+    <img
+      v-if="currentPreview == 0"
+      class="previewImg"
+      src="@/assets/images/hometab.png"
+      alt=""
+    />
+    <img
+      v-if="currentPreview == 1"
+      class="previewImg"
+      src="@/assets/images/albumsTab.png"
+      alt=""
+    />
+    <img
+      v-if="currentPreview == 2"
+      class="previewImg"
+      src="@/assets/images/artistTab.png"
+      alt=""
+    />
+    <img
+      v-if="currentPreview == 3"
+      class="previewImg"
+      src="@/assets/images/playlistTab.png"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentPreview: 0,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.currentPreview == 3) {
+        this.currentPreview = 0;
+      } else {
+        this.currentPreview += 1;
+      }
+    }, 2000);
+  },
+};
+</script>
+
+<style lang="scss">
+.preview {
+  position: relative;
+  width: 50vw;
+  #laptop {
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .previewImg {
+    position: relative;
+    z-index: 3;
+    width: 77%;
+    margin-top: -15px;
+  }
+  @media (max-width: 700px) {
+    width: 90vw;
+    .previewImg {
+      margin-top: 0px;
+    }
+  }
+}
+</style>
