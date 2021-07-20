07a7d8b022c52dd13d1a8fd58f71bfaa0c563b39
Patrick <56973177+Patrick-web@users.noreply.github.com>
2021-07-20 12:17:21 +0300

    1.1.7  release for win and appimage |  removed other links

--- src/App.vue
+++ src/App.vue
@@ -23,13 +23,13 @@
       <h3>Download</h3>
       <div class="btns">
         <a
-          href="https://github.com/Patrick-web/FLB-Music-Player-Official/releases/download/v1.1.6/FLB-Music-Setup-1.1.6.exe"
+          href="https://github.com/Patrick-web/FLB-Music-Player-Official/releases/download/v1.1.7/FLB-Music-Setup-1.1.7.exe"
         >
           <img src="@/assets/images/windows.svg" alt="" />
           <p>Windows</p>
         </a>
         <a
-          href="https://github.com/Patrick-web/FLB-Music-Player-Official/releases/download/v1.1.6/FLB-Music-1.1.6.AppImage"
+          href="https://github.com/Patrick-web/FLB-Music-Player-Official/releases/download/v1.1.7/FLB-Music-1.1.7.AppImage"
         >
           <img src="@/assets/images/linux.svg" alt="" />
           <p>AppImage</p>
